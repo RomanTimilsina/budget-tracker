9896269bbe412d2d6ede5ae7d054ea86eaa77b39
Roman <romantimilsina@gmail.com>
2022-12-14 18:28:32 +0545

    budgetCard-cmponent-initiated

---
+++ src/component/BudgetCard.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Card } from 'react-bootstrap'
+import { currencyFormatter } from '../utils'
+
+export default function BudgetCard({name,amount,max}) {
+  return (
+    <div>
+      <Card>
+        <Card.Body>
+          <Card.Title>
+           <div>{name}</div>
+           <div>{currencyFormatter.format(amount)}/{currencyFormatter.format(max)}</div>
+          </Card.Title>
+        </Card.Body>
+      </Card>
+    </div>
+  )
+}
